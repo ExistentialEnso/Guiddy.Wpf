bb0470b1188dc00142ccd7b63d17a8ab9709d827
Thorne Melcher <tmelcher@topofmind.com>
2015-08-31 12:39:01 -0400

    Add unit test project with two tests.

---
+++ Guiddy.Wpf.Tests/GuidGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Guiddy.Wpf.Tests
+{
+    [TestFixture]
+    public class GuidGeneratorTests
+    {
+        [Test]
+        public void If_Uppercase_True_Then_Result_Should_Be_All_Uppercase()
+        {
+            var hyphenated = GuidGenerator.Generate(true, true);
+            var nonHyphenated = GuidGenerator.Generate(false, true);
+
+            Assert.AreEqual(hyphenated, hyphenated.ToUpper());
+            Assert.AreEqual(nonHyphenated, nonHyphenated.ToUpper());
+        }
+
+        [Test]
+        public void If_Uppercase_False_Then_Result_Should_Be_All_Lowercase()
+        {
+            var hyphenated = GuidGenerator.Generate(true, false);
+            var nonHyphenated = GuidGenerator.Generate(false, false);
+
+            Assert.AreEqual(hyphenated, hyphenated.ToLower());
+            Assert.AreEqual(nonHyphenated, nonHyphenated.ToLower());
+        }
+    }
+}
